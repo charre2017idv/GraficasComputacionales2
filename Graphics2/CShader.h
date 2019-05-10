60e5a0e7e26ad91ff1540dee40c017b25aa43725
charre2017idv <idv17c.rcharreton@uartesdigitales.edu.mx>
2019-05-10 17:25:31 -0500

    Third Bunch of Data

---
+++ Graphics2/CShader.h
@@ -0,0 +1,36 @@
+#pragma once
+#include "Header.h"
+#if defined(DIRECTX)
+#elif defined(OPENGL)
+
+class CShader
+{
+public:
+	CShader(const string &file);
+	~CShader();
+private:
+	string m_path;
+	// Catch the uniforms
+	unordered_map <string, int> m_uniformLocationCatche;
+public:
+	unsigned int m_handle_ID;
+	void Bind() const;
+	void UnBind() const;
+
+public:
+	// Set Uniforms
+	void setUniform1i(const string &name, int value); // Texture
+	void setUniform3f(const string &name, float x, float y, float z); // Lambert
+	void setUniform4f(const string &name, float x, float y, float z, float w); // Albedo
+	void setUniform4fv(const string & name, int amount, float x[]);
+	void setUniformMat4f(const string &name, const glm::mat4 matrix4); // Albedo
+
+private:
+	ShaderSources parseShader(const string & file);
+	unsigned int  compileShader(unsigned int type, const string & source);
+	unsigned int  createShader(const string &vertexShader, const string &pixelShader);
+
+	int GetUniformLocatiiion(const string &name);
+
+};
+#endif
